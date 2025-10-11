8432475588a77d81c0f42caa65bbdf7d1a94e103
Kamil-Salih <C00307549@setu.ie>
2025-10-11 11:57:01 +0000

    added group detail in header, gave the log file the variable logfile, added code that lets you parse the log file line by line

--- assignment1.py
+++ assignment1.py
@@ -2,7 +2,15 @@
 Title: Log File Analysis and Threat Detection
 Student name: Kamil Salih
 Student number: C00307549
+Group: CW_KCCYB_B
 Last updated: 11/10/2025
 Description: A Python script that automates detection of common attack patterns in a log file.
 '''
 
+logfile= "CA1_project.log"
+
+#1.	Parse log file line by line
+
+with open(logfile, 'r') as f:
+    for line in f:
+        print(line.strip())
