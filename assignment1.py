6b0da7b93b4e1873a60e8734e7eaf092b8479d38
Kamil-Salih <C00307549@setu.ie>
2025-10-11 11:26:57 +0000

    added header comments

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+'''
+Title: Log File Analysis and Threat Detection
+Student name: Kamil Salih
+Student number: C00307549
+Last updated: 11/10/2025
+Description: A Python script that automates detection of common attack patterns in a log file.
+'''
+
